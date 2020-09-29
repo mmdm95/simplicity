c92fbc23f341b2980b486fe6aaad9810e823d558
MMDM <mmdehghan95@gmail.com>
2020-09-29 22:43:40 +0330

    autoload path simplified

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 use Sim\Core\Bootstrap;
 
-include_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $bootstrap = new Bootstrap();
