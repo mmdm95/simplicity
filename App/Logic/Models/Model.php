39a79ed507b1a52dbc5d5bf028cc487023aa6f56
MMDM <mmdehghan95@gmail.com>
2020-10-19 22:46:27 +0330

    add a test method

--- App/Logic/Models/Model.php
+++ App/Logic/Models/Model.php
@@ -4,5 +4,12 @@ namespace App\Logic\Models;
 
 class Model extends BaseModel
 {
+    public function test()
+    {
+        $select = $this->connector->select();
 
+        $select->cols(['*'])->from('faq');
+
+        return $this->db->fetchAll($select->getStatement(), $select->getBindValues());
+    }
 }
