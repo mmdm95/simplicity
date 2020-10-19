e6c84013faabb907c8380629bb3b217ca15ececf
MMDM <mmdehghan95@gmail.com>
2020-10-19 22:42:27 +0330

    add two auth with namespace as an example

--- App/logic/Container.php
+++ App/logic/Container.php
@@ -2,7 +2,10 @@
 
 namespace App\Logic;
 
+use Sim\Auth\Interfaces\IAuth;
 use Sim\Interfaces\IInitialize;
+use Sim\Container\Container as Resolver;
+use Sim\Auth\DBAuth as Auth;
 
 class Container implements IInitialize
 {
@@ -11,6 +14,32 @@ class Container implements IInitialize
      */
     public function init()
     {
+        // home authentication class
+        \container()->set('auth_home', function () {
+            $authConfig = \config()->get('auth');
 
+            $auth = new Auth(\connector()->getPDO(), 'home', [
+                'main' => \config()->get('env.APP_MAIN_KEY'),
+                'assured' => \config()->get('env.APP_ASSURED_KEY'),
+            ], PASSWORD_BCRYPT, IAuth::STORAGE_DB, $authConfig['structure']);
+
+            $auth->setExpiration($authConfig['expire_time'])->setSuspendTime($authConfig['suspend_time']);
+
+            return $auth;
+        });
+
+        // admin authentication class
+        \container()->set('auth_admin', function () {
+            $authConfig = \config()->get('auth');
+
+            $auth = new Auth(\connector()->getPDO(), 'admin', [
+                'main' => \config()->get('env.APP_MAIN_KEY'),
+                'assured' => \config()->get('env.APP_ASSURED_KEY'),
+            ], PASSWORD_BCRYPT, IAuth::STORAGE_DB, \config()->get('auth.structure'));
+
+            $auth->setExpiration($authConfig['expire_time'])->setSuspendTime($authConfig['suspend_time']);
+
+            return $auth;
+        });
     }
 }
