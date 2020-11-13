96cea7def75aabc9937966d46d8359692bf058a7
MMDM <mmdehghan95@gmail.com>
2020-11-13 15:13:59 +0330

    can send data to other files too

--- vendor/simplicity-framework/Functions/loadHelper.php
+++ vendor/simplicity-framework/Functions/loadHelper.php
@@ -1,15 +1,15 @@
 <?php
 
 if (!function_exists('load_view')) {
-    function load_view(string $filename, ?string $ext = 'php', int $type = \Sim\Loader\Loader::TYPE_INCLUDE)
+    function load_view(string $filename, array $data = [], ?string $ext = 'php', int $type = \Sim\Loader\Loader::TYPE_INCLUDE)
     {
-        \loader()->load(view_path($filename, false), $ext, $type);
+        \loader()->setData($data)->load(view_path($filename, false), $ext, $type);
     }
 }
 
 if (!function_exists('load_partial')) {
-    function load_partial(string $filename, ?string $ext = 'php', int $type = \Sim\Loader\Loader::TYPE_INCLUDE)
+    function load_partial(string $filename, array $data = [], ?string $ext = 'php', int $type = \Sim\Loader\Loader::TYPE_INCLUDE)
     {
-        \loader()->load(partial_path($filename, false), $ext, $type);
+        \loader()->setData($data)->load(partial_path($filename, false), $ext, $type);
     }
 }
